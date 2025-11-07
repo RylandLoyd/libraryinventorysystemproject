379523ebb438a0305bca3f786592582b714a62e3
axmiye1027 <axmiye1027@gmail.com>
2025-11-07 14:28:07 -0800

    Create Library.h

---
+++ Library.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+ * AUTHOR			: Amy X, Ryland L, Cristal D, Elizabeth S, Paul S.
+ * PROJECT #2		: Library Inventory System
+ * CLASS			: CS 1C
+ * SECTION			: TTh 8a
+ * DUE DATE			: ...
+ **************************************************************************/
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <iostream>		/* cin, cout		*/
+#include <iomanip>		/* setw				*/
+#include <string>		/* string class		*/
+#include <vector>		/* vector STL	 	*/
+#include <sstream>		/* ostringstream 	*/
+using namespace std;
+
+/***************************************************************************
+ * CLASS Library
+ * -------------------------------------------------------------------------
+ * This class represents a single library system that can store items.
+ **************************************************************************/
+class Library
+{
+private:
+	//libraryInventory[][]
+
+
+};
+
+
+
+#endif
